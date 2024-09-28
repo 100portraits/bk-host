1e6cedc25d86129c57ca40122debd52a55da3cec
sahir <sahirde@gmail.com>
2024-09-29 00:04:17 +0200

    passkey

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
 
 //get the password from the environment variable in Cloudflare Pages
-export const LOGIN_PASSWORD = process.env.REACT_APP_LOGIN_PASSWORD;
+export const LOGIN_PASSWORD = process.env.CODE;
