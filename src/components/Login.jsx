9ffe65db3ae66aeeb246353a402ffc515a164bed
sahir <sahirde@gmail.com>
2024-09-29 00:03:02 +0200

    passkey

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,6 +14,8 @@ const Login = ({ onLogin }) => {
     }
   };
 
+  console.log(LOGIN_PASSWORD)
+  
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded shadow-md">
