385b4ff7f5b01ed75de1592d026bd274ce227daa
sahir <sahirde@gmail.com>
2024-10-15 11:51:07 +0200

    vite2

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,9 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-
-// Conditionally import getAnalytics
-let getAnalytics;
-if (typeof window !== 'undefined') {
-  getAnalytics = (await import('firebase/analytics')).getAnalytics;
-}
+import { getAuth } from "firebase/auth"; // Add this import
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -24,13 +19,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
-const auth = getAuth(app);
+const auth = getAuth(app); // Initialize auth
 
-// Conditionally initialize analytics
-let analytics;
-if (typeof window !== 'undefined' && getAnalytics) {
-  analytics = getAnalytics(app);
-}
-
-export { app, db, auth, analytics };
+export { app, db, auth };
